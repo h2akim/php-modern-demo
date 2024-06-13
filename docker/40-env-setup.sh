f4f48343869223446e857dbf9f5e15ed133664f8
Hakim Razalan <hakim.razalan@gmail.com>
2024-06-13 20:49:01 +0800

    fix env setup

--- docker/40-env-setup.sh
+++ docker/40-env-setup.sh
@@ -3,6 +3,6 @@
 # set database settings
 sed -i "s|DB_HOST=|DB_HOST=$DB_HOST|g" /var/www/html/.env
 sed -i "s|DB_PORT=|DB_PORT=$DB_PORT|g" /var/www/html/.env
-sed -i "s|'DB_DATABASE=/var/www/html/database/database.sqlite'|DB_DATABASE=$DB_DATABASE|g" /var/www/html/.env
+sed -i "s|DB_DATABASE=|DB_DATABASE=$DB_DATABASE|g" /var/www/html/.env
 sed -i "s|DB_USERNAME=|DB_USERNAME=$DB_USERNAME|g" /var/www/html/.env
 sed -i "s|DB_PASSWORD=|DB_PASSWORD=$DB_PASSWORD|g" /var/www/html/.env
