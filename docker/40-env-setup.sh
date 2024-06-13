2f55d6a4543ff27136d933b1290e0fd092995fcd
Hakim Razalan <hakim.razalan@gmail.com>
2024-06-13 18:17:13 +0800

    try see does it populate db data

--- docker/40-env-setup.sh
+++ docker/40-env-setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # set database settings
-ls
 sed -i "s|DB_CONNECTION=sqlite|DB_HOST=$DB_CONNECTION|g" /var/www/html/.env
 sed -i "s|DB_HOST=|DB_HOST=$DB_HOST|g" /var/www/html/.env
 sed -i "s|DB_PORT=|DB_PORT=$DB_PORT|g" /var/www/html/.env
 sed -i "s|DB_DATABASE=/var/www/html/database/database.sqlite|DB_DATABASE=$DB_DATABASE|g" /var/www/html/.env
 sed -i "s|DB_USERNAME=|DB_USERNAME=$DB_USERNAME|g" /var/www/html/.env
 sed -i "s|DB_PASSWORD=|DB_PASSWORD=$DB_PASSWORD|g" /var/www/html/.env
+
+cat /var/www/html/.env
